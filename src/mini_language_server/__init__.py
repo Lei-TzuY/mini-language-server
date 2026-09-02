2828d0edd25dd588e1a41f36bf0ad9e99c1cd21b
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-09-02 20:49:57 +0800

    feat: expose document store API

--- src/mini_language_server/__init__.py
+++ src/mini_language_server/__init__.py
@@ -1,9 +1,13 @@
 """mini-language-server package."""
 
+from .documents import Document, DocumentError, DocumentStore
 from .protocol import FramingError, JsonRpcError, MessageReader, encode_message
 from .server import LanguageServer, ServerState
 
 __all__ = [
+    "Document",
+    "DocumentError",
+    "DocumentStore",
     "FramingError",
     "JsonRpcError",
     "LanguageServer",
