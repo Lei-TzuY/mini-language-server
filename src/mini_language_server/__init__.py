647e5b1e71bb3fd2b2b1d6ad85eedce160f34cac
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-09-03 02:49:22 +0800

    feat: expose syntax snapshot API

--- src/mini_language_server/__init__.py
+++ src/mini_language_server/__init__.py
@@ -4,6 +4,7 @@ from .documents import Document, DocumentError, DocumentStore
 from .protocol import FramingError, JsonRpcError, MessageReader, encode_message
 from .server import LanguageServer, ServerState
 from .source import Position, SourceError, SourceText, Span
+from .syntax import SyntaxError, SyntaxSnapshot, SyntaxStore
 
 __all__ = [
     "Document",
@@ -18,5 +19,8 @@ __all__ = [
     "SourceError",
     "SourceText",
     "Span",
+    "SyntaxError",
+    "SyntaxSnapshot",
+    "SyntaxStore",
     "encode_message",
 ]
