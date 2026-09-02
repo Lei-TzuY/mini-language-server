22763e6c474b0c37bf84a9a6475ae78c8c1fc2ae
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-09-03 00:52:15 +0800

    feat: expose source coordinate API

--- src/mini_language_server/__init__.py
+++ src/mini_language_server/__init__.py
@@ -3,6 +3,7 @@
 from .documents import Document, DocumentError, DocumentStore
 from .protocol import FramingError, JsonRpcError, MessageReader, encode_message
 from .server import LanguageServer, ServerState
+from .source import Position, SourceError, SourceText, Span
 
 __all__ = [
     "Document",
@@ -12,6 +13,10 @@ __all__ = [
     "JsonRpcError",
     "LanguageServer",
     "MessageReader",
+    "Position",
     "ServerState",
+    "SourceError",
+    "SourceText",
+    "Span",
     "encode_message",
 ]
