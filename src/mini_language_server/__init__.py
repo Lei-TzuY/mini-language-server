6630a5df43d17fd54b7f24d3f23addd451f9fe35
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-09-07 20:56:37 +0800

    feat: expose range-formatting product server

--- src/mini_language_server/__init__.py
+++ src/mini_language_server/__init__.py
@@ -9,7 +9,7 @@ from .cancellation import (
 )
 from .diagnostics import Diagnostic, DiagnosticError, DiagnosticSnapshot, DiagnosticStore
 from .documents import Document, DocumentError, DocumentStore
-from .on_type_formatting import NovaProductLanguageServer
+from .range_formatting import NovaProductLanguageServer
 from .protocol import FramingError, JsonRpcError, MessageReader, encode_message
 from .semantic import Reference, SemanticDatabase, SemanticError, SemanticSnapshot
 from .server import LanguageServer, ServerState
