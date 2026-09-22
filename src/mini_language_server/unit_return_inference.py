0801551ca8f3228f1f076bd43a53bd4f08a25652
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-09-23 02:20:16 +0800

    fix(lsp): drop obsolete Unit return regex import

--- src/mini_language_server/unit_return_inference.py
+++ src/mini_language_server/unit_return_inference.py
@@ -4,7 +4,6 @@ from __future__ import annotations
 
 from typing import Any
 
-from .inferred_function_returns import _RETURN
 from .unit_type_actions import NovaProductLanguageServer as _NovaProductLanguageServer
 
 
